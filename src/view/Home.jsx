8f66c4e5d3a7d4c1e81a9c6564d42e21fad546d2
Andrew Domingues <drewdomi@proton.me>
2023-07-27 15:03:45 -0400

    feat: add Logo

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,10 +1,12 @@
 import PolaroidsGrid from "../components/PolaroidsGrid";
+import Logo from "../components/Logo";
 
 function Home() {
   return (
-    <>
+    <main>
+      <Logo />
       <PolaroidsGrid />
-    </>
+    </main>
   )
 }
 
