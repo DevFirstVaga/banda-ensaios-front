b0fb245894c176b2fce9e368430f807c52c66803
Andrew Domingues <drewdomi@proton.me>
2023-08-04 12:36:19 -0300

    feat: add HomeLinks

--- src/components/Link/styled.js
+++ src/components/Link/styled.js
@@ -1,18 +1,39 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import theme from "../../theme/theme"
 
 export const Wrapper = styled.div`
-  background-color: ${theme.colors.gray};
-  height: 300px;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  a{
-    color: ${theme.colors.white};
-    font-size: ${theme.fonts.size.title};
-    font-family: ${theme.fonts.display};
-    text-decoration: none;
-
-  }
+  ${({ photo }) => css`
+    background-color: ${theme.colors.gray};
+    background-image: url("/src/images/tour.jpeg");
+    background-color: ${theme.colors.blackTransp};
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    height: 400px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &::after{
+        display: block;
+        opacity: .5;
+        content: "";
+        position: absolute;
+        background-color: ${theme.colors.black0};
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+      }
+    a{
+      color: ${theme.colors.white};
+      font-size: ${theme.fonts.size.link};
+      font-family: ${theme.fonts.display};
+      text-transform: uppercase;
+      text-decoration: none;
+      text-shadow: 0px 0px 4px black;
+      position: absolute;
+      z-index: 1;
+    }
+  `}
 `
