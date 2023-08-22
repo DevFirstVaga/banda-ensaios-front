73c7e9bfa0fd1a3c54a077ee6abca3bdbbfc996f
Andrew Domingues <drewdomi@proton.me>
2023-08-21 21:49:45 -0300

    feat: add quote component

---
+++ src/components/Quote/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import theme from "../../theme/theme";
+
+export const Wrapper = styled.div`
+  margin: 40px auto 30px;
+  width: 260px;
+  font-family: ${theme.fonts.display};
+  font-size: 18px;
+  
+`
--- src/view/Sobre.jsx
+++ src/view/Sobre.jsx
@@ -1,13 +1,21 @@
 import Banner from "../components/Banner";
+import Quote from "../components/Quote";
 
-function Sobre(){
-  return(
+function Sobre() {
+  return (
     <>
       <Banner
         image="/images/links/sobre-a-banda.jpg"
       />
+      <Quote quote={
+        <>
+          <strong>É preciso muito amor pra poder pulsar. É preciso paz pra poder sorrir. É preciso a chuva para florir."</strong> Cazuza (Música: "O Tempo Não Para")
+        </>
+      } />
+
+      
     </>
-  )
+  );
 }
 
-export default Sobre
+export default Sobre;
