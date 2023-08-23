d5d4a3176830c6dd8b7169f5dfd41e5cd837b6c3
Andrew Domingues <drewdomi@proton.me>
2023-08-22 22:25:47 -0300

    feat: implements Paragraph

--- src/components/AboutImgAndQuote/Quote/styled.js
+++ src/components/AboutImgAndQuote/Quote/styled.js
@@ -5,5 +5,8 @@ export const Wrapper = styled.div`
   margin: 40px auto 30px;
   width: 260px;
   font-family: ${theme.fonts.display};
-  font-size: 18px; 
+  font-size: 18px;
+  span{
+    font-weight: 100;
+  }
 `
